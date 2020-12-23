74a0087f574552f5f9da3f37fe1341d661adfbca
tobinski <tobinski@dodis.ch>
2020-12-23 13:10:34 +0100

    Update command and add better cli interface

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -14,95 +12,65 @@ import (
 func main() {
 	arg := os.Args
 	if len(arg) < 2 {
-		fmt.Println("Please provide a command.")
-		fmt.Println("beamer list : list all available serial ports")
-		fmt.Println("beamer start [port] : start beamer on serial port")
-		fmt.Println("beamer stop [port] : stop beamer on serial port")
+		log.Println("Please provide a command.")
+		log.Println("beamer list : list all available serial ports")
+		log.Println("beamer start [port] : start beamer on serial port")
+		log.Println("beamer stop [port] : stop beamer on serial port")
 		return
 	}
 	cmd := arg[1]
 	switch cmd {
 	case "list":
 		listPorts()
-	case "power":
-		if len(arg) < 3 {
-			fmt.Println("Error: No port declared")
-			fmt.Println("beamer start [port]")
-			return
-		}
-		readPowerState(arg[2])
 		case "start":
-
 		if len(arg) < 3 {
-			fmt.Println("Error: No port declared")
-			fmt.Println("beamer start [port]")
+			log.Println("Error: No port declared")
+			log.Println("beamer start [port]")
 			return
 		}
 		startBeamer(arg[2])
 	case "stop":
 		if len(arg) < 3 {
-			fmt.Println("Error: No port declared")
-			fmt.Println("beamer start [port]")
+			log.Println("Error: No port declared")
+			log.Println("beamer start [port]")
 			return
 		}
 		stopBeamer(arg[2])
 	default:
-		fmt.Println("Please provide a valid command.")
-		fmt.Println("beamer list : list all available serial ports")
-		fmt.Println("beamer start [port] : start beamer on serial port")
-		fmt.Println("beamer stop [port] : stop beamer on serial port")
+		log.Println("Please provide a valid command.")
+		log.Println("beamer list : list all available serial ports")
+		log.Println("beamer start [port] : start beamer on serial port")
+		log.Println("beamer stop [port] : stop beamer on serial port")
 	}
 }
 
 func listPorts() {
-	fmt.Println("List all available serial ports")
+	log.Println("List all available serial ports")
 	ports, err := enumerator.GetDetailedPortsList()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if len(ports) == 0 {
-		fmt.Println("No serial ports found!")
+		log.Println("No serial ports found!")
 		return
 	}
 	for _, port := range ports {
-		fmt.Printf("Found port: %s\n", port.Name)
+		log.Printf("Found port: %s\n", port.Name)
 		if port.IsUSB {
-			fmt.Printf("USB ID     %s:%s\n", port.VID, port.PID)
-			fmt.Printf("USB serial %s\n", port.SerialNumber)
+			log.Printf("USB ID     %s:%s\n", port.VID, port.PID)
+			log.Printf("USB serial %s\n", port.SerialNumber)
 		}
 	}
 }
 
-
 func startBeamer(port string) {
-	writToBeamer(port, []byte("~XX00 1"))
+	writToBeamer(port, []byte("~0000 1\r"))
 }
 
 func stopBeamer(port string) {
-	data, _ := hex.DecodeString("7E303030302032")
-	writToBeamer(port, data)
+	writToBeamer(port, []byte("~0000 0\r"))
 }
 
-func readPowerState(port string)  {
-	data, _ := hex.DecodeString("7E30303132342031")
-
-	mode := &serial.Mode{
-		BaudRate: 9600,
-		Parity: serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-	port1, err := serial.Open(port, mode)
-	if err != nil {
-		log.Fatal(err)
-	}
-	b, err := port1.Read(data)
-	if err != nil {
-		fmt.Println("Erro reading power state")
-		fmt.Println(err)
-	}
-	fmt.Println(string(b))
-}
 func writToBeamer(port string, data []byte) {
 	mode := &serial.Mode{
 		BaudRate: 9600,
@@ -114,23 +82,17 @@ func writToBeamer(port string, data []byte) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	status, err := port1.GetModemStatusBits()
-	if err != nil {
-		fmt.Println("Error getting status bytes")
-		return
-	}
-	fmt.Println(status)
 	b, err := port1.Write(data)
 	if err != nil {
-		fmt.Println("Could not send data to port "+port)
-		fmt.Println(err)
+		log.Println("Could not send data to port "+port)
+		log.Println(err)
 	} else {
-		fmt.Println("Send "+strconv.Itoa(b)+" bytes to port "+port)
+		log.Println("Send "+strconv.Itoa(b)+" bytes to port "+port)
 	}
 	err = port1.Close()
 	if err != nil {
-		fmt.Println("Error closing serial port")
-		fmt.Println(err)
+		log.Println("Error closing serial port")
+		log.Println(err)
 	}
-	fmt.Println("Write to beamer complete. Port closed")
+	log.Println("Port closed")
 }
